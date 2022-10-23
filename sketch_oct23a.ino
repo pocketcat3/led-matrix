7a51227f6bd7e33ad2908a6d53017df1ca00914a
pocketcat3 <114851989+pocketcat3@users.noreply.github.com>
2022-10-23 11:01:58 +0300

    Add files via upload

---
+++ sketch_oct23a.ino
@@ -0,0 +1,147 @@
+#include <LedControl.h>
+LedControl lc=LedControl(12,11,10,1);
+void setup() {
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 8);
+  lc.clearDisplay(0);
+}
+
+void loop() {
+  lc.setLed(0, 0, 0, 1);
+  delay(200);
+  lc.setLed(0, 0, 1, 1);
+  delay(200);
+  lc.setLed(0, 0, 2, 1);
+  delay(200);
+  lc.setLed(0, 0, 3, 1);
+  delay(200);
+  lc.setLed(0, 0, 4, 1);
+  delay(200);
+  lc.setLed(0, 0, 5, 1);
+  delay(200);
+  lc.setLed(0, 0, 6, 1);
+  delay(200);
+  lc.setLed(0, 0, 7, 1);
+  delay(200);
+
+  lc.setLed(0, 1, 0, 1);
+  delay(200);
+  lc.setLed(0, 1, 1, 1);
+  delay(200);
+  lc.setLed(0, 1, 2, 1);
+  delay(200);
+  lc.setLed(0, 1, 3, 1);
+  delay(200);
+  lc.setLed(0, 1, 4, 1);
+  delay(200);
+  lc.setLed(0, 1, 5, 1);
+  delay(200);
+  lc.setLed(0, 1, 6, 1);
+  delay(200);
+  lc.setLed(0, 1, 7, 1);
+  delay(200);
+
+  lc.setLed(0, 2, 0, 1);
+  delay(200);
+  lc.setLed(0, 2, 1, 1);
+  delay(200);
+  lc.setLed(0, 2, 2, 1);
+  delay(200);
+  lc.setLed(0, 2, 3, 1);
+  delay(200);
+  lc.setLed(0, 2, 4, 1);
+  delay(200);
+  lc.setLed(0, 2, 5, 1);
+  delay(200);
+  lc.setLed(0, 2, 6, 1);
+  delay(200);
+  lc.setLed(0, 2, 7, 1);
+  delay(200);
+
+  lc.setLed(0, 3, 0, 1);
+  delay(200);
+  lc.setLed(0, 3, 1, 1);
+  delay(200);
+  lc.setLed(0, 3, 2, 1);
+  delay(200);
+  lc.setLed(0, 3, 3, 1);
+  delay(200);
+  lc.setLed(0, 3, 4, 1);
+  delay(200);
+  lc.setLed(0, 3, 5, 1);
+  delay(200);
+  lc.setLed(0, 3, 6, 1);
+  delay(200);
+  lc.setLed(0, 3, 7, 1);
+  delay(200);
+
+  lc.setLed(0, 4, 0, 1);
+  delay(200);
+  lc.setLed(0, 4, 1, 1);
+  delay(200);
+  lc.setLed(0, 4, 2, 1);
+  delay(200);
+  lc.setLed(0, 4, 3, 1);
+  delay(200);
+  lc.setLed(0, 4, 4, 1);
+  delay(200);
+  lc.setLed(0, 4, 5, 1);
+  delay(200);
+  lc.setLed(0, 4, 6, 1);
+  delay(200);
+  lc.setLed(0, 4, 7, 1);
+  delay(200);
+
+  lc.setLed(0, 5, 0, 1);
+  delay(200);
+  lc.setLed(0, 5, 1, 1);
+  delay(200);
+  lc.setLed(0, 5, 2, 1);
+  delay(200);
+  lc.setLed(0, 5, 3, 1);
+  delay(200);
+  lc.setLed(0, 5, 4, 1);
+  delay(200);
+  lc.setLed(0, 5, 5, 1);
+  delay(200);
+  lc.setLed(0, 5, 6, 1);
+  delay(200);
+  lc.setLed(0, 5, 7, 1);
+  delay(200);
+
+  lc.setLed(0, 6, 0, 1);
+  delay(200);
+  lc.setLed(0, 6, 1, 1);
+  delay(200);
+  lc.setLed(0, 6, 2, 1);
+  delay(200);
+  lc.setLed(0, 6, 3, 1);
+  delay(200);
+  lc.setLed(0, 6, 4, 1);
+  delay(200);
+  lc.setLed(0, 6, 5, 1);
+  delay(200);
+  lc.setLed(0, 6, 6, 1);
+  delay(200);
+  lc.setLed(0, 6, 7, 1);
+  delay(200);
+
+  lc.setLed(0, 7, 0, 1);
+  delay(200);
+  lc.setLed(0, 7, 1, 1);
+  delay(200);
+  lc.setLed(0, 7, 2, 1);
+  delay(200);
+  lc.setLed(0, 7, 3, 1);
+  delay(200);
+  lc.setLed(0, 7, 4, 1);
+  delay(200);
+  lc.setLed(0, 7, 5, 1);
+  delay(200);
+  lc.setLed(0, 7, 6, 1);
+  delay(200);
+  lc.setLed(0, 7, 7, 1);
+  delay(200);
+  lc.clearDisplay(0);
+  delay(200);
+}
